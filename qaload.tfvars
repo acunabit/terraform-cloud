f1bff84b54055a05e9eabe3d1ab2e821380a40d8
Cesar Acuna <cesar.acuna@afterpaytouch.com>
2020-04-06 07:43:47 +1000

    TECH-1004 hack day terraform cloud

---
+++ qaload.tfvars
@@ -0,0 +1,5 @@
+instance_type = "t3a.small"
+key_pair_name = "paylater-beta"
+region = "ap-southeast-2"
+vpc_id = "vpc-0b1a2b36505745bb5"
+subnet_id = "subnet-01770c6a5af540852"
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "account_id" {
+  default = "723236915308"
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_pair_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
