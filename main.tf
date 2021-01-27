d1a7ae728b83a69dfcafe9de246be7d462856cde
Cesar Acuna <cesar.acuna@afterpaytouch.com>
2021-01-27 22:28:55 +1100

    Testing terraform operator

---
+++ main.tf
@@ -0,0 +1,17 @@
+locals {
+    terraform_configuration = ""
+}
+
+terraform {
+    required_version = "0.14.5"
+}
+
+provider "aws" {
+    region = var.region
+    assume_role {
+        role_arn = "arn:aws:iam::${var.account_id}:role/terraform-paylater-core"
+        session_name = "terraform-paylater-core-aws-provider"
+    }
+    version = "~> 2.0"
+}
+
